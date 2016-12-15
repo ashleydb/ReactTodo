59145b1e44fad029c1dfec439cada55ea75642cd
ashleydb <abennett@gmail.com>
2016-12-15 15:45:46 -0800

    Getting the ToDo checkboxes to mark items as complete
    Partial implementation...

--- app/components/Todo.jsx
+++ app/components/Todo.jsx
@@ -1,12 +1,17 @@
 var React = require('react');
 
 var Todo = React.createClass({
+  handleCompleted: function() {
+    this.props.onToggle(this.props.id);
+  },
   render: function() {
     var {id, text, time, complete} = this.props;
     return (
       <div>
-        <p>{id}. {text}</p>
-        <p>Created {time}</p>
+        <label>
+          <input type="checkbox" ref="markCompleted" onChange={this.handleCompleted} checked={complete}/> {text}
+          <p><small>Created {time}</small></p>
+        </label>
       </div>
     );
   }
